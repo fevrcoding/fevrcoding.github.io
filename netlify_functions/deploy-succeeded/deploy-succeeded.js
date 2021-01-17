8b25d55da9b6cba45ba13a33f6a8979d4d88366c
Marco Solazzi <marco.solazzi@gmail.com>
2021-01-17 17:15:18 +0900

    new event notification

--- netlify_functions/deploy-succeeded/deploy-succeeded.js
+++ netlify_functions/deploy-succeeded/deploy-succeeded.js
@@ -1,26 +1,64 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// const { ONE_SIGNAL_API_KEY, ONE_SIGNAL_APP_ID } = process.env;
+const { ONE_SIGNAL_API_KEY, ONE_SIGNAL_APP_ID } = process.env;
 
-exports.handler = function (event) {
-  console.log(event);
-  // axios.post(
-  //   'https://onesignal.com/api/v1/notifications',
-  //   {
-  //     included_segments: ['Active Users'],
-  //     app_id: ONE_SIGNAL_APP_ID,
-  //     headings: {
-  //       en: 'Pubblicato un nuovo evento!',
-  //     },
-  //     contents: {
-  //       en: "Titolo dell'evento",
-  //     },
-  //   },
-  //   {
-  //     headers: { Authorization: `Basic ${ONE_SIGNAL_API_KEY}` },
-  //   },
-  // );
-  return {
+exports.handler = async function ({ body = '{}' }) {
+  const { payload = {} } = JSON.parse(body);
+
+  const ret = {
     statusCode: 200,
   };
+
+  if (payload.branch !== 'master') {
+    return ret;
+  }
+
+  const { data } = await axios.get(
+    `https://api.github.com/repos/fevrcoding/fevrcoding.github.io/commits/${payload.commit_ref}`,
+  );
+
+  const [, eventPath] =
+    data.commit.message.match(/fevrcoding\/cms\/event\/(.+?)\n/) || [];
+
+  if (!eventPath) {
+    return ret;
+  }
+
+  const eventFile = data.files.find(
+    ({ filename, status }) =>
+      status === 'added' && filename.includes(eventPath),
+  );
+
+  if (!eventFile) {
+    return ret;
+  }
+
+  const { data: eventTxt } = await axios.get(eventFile.raw_url, {
+    responseType: 'text',
+  });
+
+  const [, eventTitle] = eventTxt.match(/title:(.+?)\n/) || [];
+
+  if (!eventTitle) {
+    return ret;
+  }
+
+  axios.post(
+    'https://onesignal.com/api/v1/notifications',
+    {
+      included_segments: ['Active Users'],
+      app_id: ONE_SIGNAL_APP_ID,
+      headings: {
+        en: 'Nuovo evento!',
+      },
+      contents: {
+        en: eventTitle,
+      },
+      web_url: 'https://www.fevr.it/',
+    },
+    {
+      headers: { Authorization: `Basic ${ONE_SIGNAL_API_KEY}` },
+    },
+  );
+  return ret;
 };
