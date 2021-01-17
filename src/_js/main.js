c7fca2c544029fd4b2c8988640a59c75b2409d41
Marco Solazzi <marco.solazzi@gmail.com>
2021-01-17 17:32:51 +0900

    force subscribe btn visibility

--- src/_js/main.js
+++ src/_js/main.js
@@ -26,8 +26,8 @@ OneSignal.push([
   'getNotificationPermission',
   (permission) => {
     const isPushSupported = OneSignal.isPushNotificationsSupported();
-    if (!isPushSupported || permission !== 'default') {
-      document.getElementById('notification-bar').hidden = true;
+    if (isPushSupported && permission === 'default') {
+      document.getElementById('notification-bar').hidden = false;
     }
     OneSignal.on('notificationPermissionChange', () => {
       document.getElementById('notification-bar').hidden = true;
